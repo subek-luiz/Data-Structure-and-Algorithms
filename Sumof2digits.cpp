9f6949a857c0a78400030f86122d0e3714206d6a
Subek Acharya <subekacharya@gmail.com>
2024-07-13 22:54:41 +0545

    Basics of java before starting DSA problems

---
+++ Sumof2digits.cpp
@@ -0,0 +1,15 @@
+// Sum of two digits
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, sum;
+    std::cin >> a;
+    std::cin >> b;
+
+    sum = a + b;
+    cout << sum;
+    
+    return 0;
+}
